559e76d5eb460b2777251e9bf1f7bcd8243be203
UnnamedMistress <momchrysti@gmail.com>
2023-01-10 08:55:24 -0500

    Current day added

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,3 @@
-// 1. Get the city value from the form
-// 2. Pass the city to the geoCode function
-// 3. Pass the Lat and Long to the GetWeather function
-// 4. Build HTML with the Data we get from the weather
 let city = document.querySelector('#city-search');
 let submitBtn = document.querySelector('#submitbtn')
 const key = 'e16d7ce165cee00c3693cb78fb283a63';
@@ -18,6 +14,7 @@ function getCity (event) {
     if (cityName) {
         
         getLat(cityName);
+        
         storedCities(cityName);
         error.innerHTML = ""
     } else {
@@ -43,9 +40,11 @@ function getLat(cityName) {
     return response.json();
   })
   .then(function (data) {
+    getDailyWeather(data[0].lat,data[0].lon);
     getWeather(data[0].lat,data[0].lon)
     console.log(data)
   });}
+  
 
 function getWeather(lat,lon){
   console.log(lat,lon);
@@ -55,33 +54,30 @@ function getWeather(lat,lon){
     return response.json();
   })
   .then(function (weather) {
-    console.log(weather)
+  
     let date = weather.list[0].dt_txt
-console.log(date);
-document.querySelector('#card1').append(`Date: ${date}`);
-document.querySelector('#card1').style.fontWeight = 'bold';
     //getting the Icon and make an image with it..
     
     for (let i = 1; i < 5; i++) {
         let date = weather.list[i].dt_txt;
-        console.log(date);
+        
         document.querySelector(`#card${i}`).append(`Date: ${date}`);
         document.querySelector(`#card${i}`).style.fontWeight = 'bold';
       
         let temp = weather.list[i].main.temp;
         document.querySelector(`#card${i}-temp`).append(`Temp: ${temp}`);
-        console.log(temp);
+       
       
         let humidity = weather.list[i].main.humidity;
         document.querySelector(`#card${i}-humid`).append(`Humidity: ${humidity}`);
-        console.log(humidity);
+      
       
         let wind = weather.list[i].wind.speed;
         document.querySelector(`#card${i}-wind`).append(`Wind Speed: ${wind}`);
-        console.log(wind);
+       
       
         let iconData = weather.list[i].weather[0].icon;
-        console.log(iconData);
+       
         const iconBaseUrl = "http://openweathermap.org/img/wn/";
     document.querySelector(`#card${i}-icon`).src = `${iconBaseUrl}${iconData}@2x.png`;
 
@@ -90,6 +86,29 @@ document.querySelector('#card1').style.fontWeight = 'bold';
       
  });
 }
+function getDailyWeather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (dailyweather) {
+  console.log(dailyweather);
+  let ctemp = dailyweather.main.temp;
+  document.querySelector(`#temp`).append(` ${ctemp}`);
+  let cwind = dailyweather.wind.speed;
+  document.querySelector(`#wind`).append(` ${cwind}`);
+  let chumidity = dailyweather.main.humidity;
+  document.querySelector(`#humid`).append(` ${chumidity}`);
+  let high = dailyweather.main.temp_max;
+  document.querySelector(`#high`).append(` ${high}`);
+  let ciconData = dailyweather.weather[0].icon;
+       console.log(ciconData);
+  const iconBaseUrl = "http://openweathermap.org/img/wn/";
+document.querySelector(`#current-icon`).src = `${iconBaseUrl}${ciconData}@2x.png`;
+        
+   });
+  }
 
 
 
